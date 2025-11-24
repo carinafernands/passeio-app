087105f69f1b1b11ff31e61e297cec54860394d2
Carina Fernandes <carina.fernandes@outlook.com>
2025-11-24 20:09:36 -0300

    eventos de rotas

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs'
 
 @Component({
   selector: 'app-layout',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  props: LayoutProps = { titulo: '', subtitulo: ''};
 
+  constructor (
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(){
+    this.router.events
+      .pipe(
+        filter( () => this.activatedRoute.firstChild !== null),
+        map( () => this.obterPropriedadeLayout() )
+      ).subscribe( (props: LayoutProps) => this.props = props)
+  }
+
+  obterPropriedadeLayout() : LayoutProps{
+    let rotaFilha = this.activatedRoute.firstChild;
+
+    while(rotaFilha?.firstChild){
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
